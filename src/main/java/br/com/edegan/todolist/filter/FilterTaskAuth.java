b54a4de3049ffce8bd3fc09d301172c65e5670e8
Eduardo Degan <edegan@furb.br>
2023-10-12 15:59:45 -0300

    Aula 04 - Atualizando tarefas e validação de rotas

--- src/main/java/br/com/edegan/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/edegan/todolist/filter/FilterTaskAuth.java
@@ -23,29 +23,37 @@ public class FilterTaskAuth extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    var authorization = request.getHeader("Authorization");
 
-    var authEncoded = authorization.substring("Basic".length()).trim();
+    var servletPath = request.getServletPath();
 
-    byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+    if (servletPath.startsWith("/tasks/")) {
 
-    var authString = new String(authDecoded);
+      var authorization = request.getHeader("Authorization");
 
-    String[] credentials = authString.split(":");
-    String username = credentials[0];
-    String password = credentials[1];
+      var authEncoded = authorization.substring("Basic".length()).trim();
 
-    var user = this.userRepository.findByUsername(username);
-    if (user == null) {
-      response.sendError(401);
-    } else {
-      var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
-      if (passwordVerify.verified) {
-        filterChain.doFilter(request, response);
-      } else {
+      byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+
+      var authString = new String(authDecoded);
+
+      String[] credentials = authString.split(":");
+      String username = credentials[0];
+      String password = credentials[1];
+
+      var user = this.userRepository.findByUsername(username);
+      if (user == null) {
         response.sendError(401);
+      } else {
+        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+        if (passwordVerify.verified) {
+          request.setAttribute("idUser", user.getId());
+          filterChain.doFilter(request, response);
+        } else {
+          response.sendError(401);
+        }
       }
+    } else {
+      filterChain.doFilter(request, response);
     }
   }
-
 }
--- src/main/java/br/com/edegan/todolist/task/TaskController.java
+++ src/main/java/br/com/edegan/todolist/task/TaskController.java
@@ -1,10 +1,22 @@
 package br.com.edegan.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.edegan.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,10 +26,39 @@ public class TaskController {
   private ITaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel taskModel) {
-    System.out.println("Chegou no controller");
+  public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    var idUser = request.getAttribute("idUser");
+    taskModel.setIdUser((UUID) idUser);
+
+    var currentDate = LocalDateTime.now();
+    if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Task cannot be scheduled in the past or have already ended.");
+    }
+    if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Task start time cannot be after end time.");
+    }
     var task = this.taskRepository.save(taskModel);
-    return task;
+    return ResponseEntity.status(HttpStatus.OK).body(task);
+
+  }
+
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request) {
+    var idUser = request.getAttribute("idUser");
+    var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+    return tasks;
+  }
+
+  @PutMapping("/{id}")
+  public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    Utils.copyNonNullProperties(taskModel, task);
+
+    return this.taskRepository.save(task);
 
   }
 
